14a73580486e48b42f33f812a89fc540119c626f
polobymulberry <jianxiongpan@gmail.com>
2018-08-29 01:07:29 +0800

    add SingletonPattern

--- SingletonPattern/main.cpp
+++ SingletonPattern/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+﻿#include <iostream>
+
+class Singleton {
+public:
+  static Singleton getInstance() {
+    static Singleton instance;
+    return instance;
+  }
+
+  void use() { std::cout << "I am singleton" << std::endl; }
+
+private:
+  Singleton() {}
+};
 
 int main() {
   std::cout << "Singleton Pattern" << std::endl;
+
+  Singleton::getInstance().use();
+
   return 0;
 }
